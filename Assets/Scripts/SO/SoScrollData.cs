fa16ba72696edc0c7d29866696cf239e190a8148
Dmitry <platelen@gmail.com>
2024-01-12 11:53:41 +0300

    SO
    Добавил SOData

---
+++ Assets/Scripts/SO/SoScrollData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SO
+{
+    [CreateAssetMenu(menuName = "ScrollData")]
+    public class SoScrollData:ScriptableObject
+    {
+        [SerializeField] private float _startSpeed = 100f;
+        [SerializeField] private float _maxSpeed = 1000f;
+        [SerializeField] private float _accelerationTime = 4f;
+
+        public float StartSpeed => _startSpeed;
+
+        public float MaxSpeed => _maxSpeed;
+
+        public float AccelerationTime => _accelerationTime;
+    }
+}
